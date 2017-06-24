eaf12d1fa90035bdf06bde51d89e2f291610c1f0
Godswill Enyoisi <tiwa.moni001@gmail.com>
2017-06-24 11:37:10 +0100

    first test

---
+++ src/test/java/AutoTest/Cucumber_Test/ist_login_test.java
@@ -0,0 +1,34 @@
+package AutoTest.Cucumber_Test;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ist_login_test {
+	
+	@Given("^am on the Landing Page$")
+	public void am_on_the_Landing_Page() throws Throwable {
+	   
+	}
+
+	@Given("^I enter my Username as wegit$")
+	public void i_enter_my_Username_as_wegit() throws Throwable {
+	  
+	}
+
+	@Given("^I enter my Password as (\\d+)$")
+	public void i_enter_my_Password_as(int arg1) throws Throwable {
+	    
+	}
+
+	@When("^I click Login button$")
+	public void i_click_Login_button() throws Throwable {
+	    
+	}
+
+	@Then("^am on the Home Page$")
+	public void am_on_the_Home_Page() throws Throwable {
+	    
+	}
+
+}
